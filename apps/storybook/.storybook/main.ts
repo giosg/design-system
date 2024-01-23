3457d96a8e6f64161f7a3902f8205da452b17a70
Igor Kornilov <gaber32188@gmail.com>
2024-01-23 10:53:00 +0200

    initial bootstrap

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,37 @@
+import { dirname, join, resolve } from "path";
+
+function getAbsolutePath(value) {
+	return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config = {
+	stories: ["../stories/**/*.stories.tsx"],
+	addons: [
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@storybook/addon-docs"),
+	],
+	framework: {
+		name: getAbsolutePath("@storybook/react-vite"),
+		options: {},
+	},
+
+	core: {},
+
+	async viteFinal(config, { configType }) {
+		// customize the Vite config here
+		return {
+			...config,
+			define: { "process.env": {} },
+			resolve: {
+				alias: [],
+			},
+		};
+	},
+
+	docs: {
+		autodocs: true,
+	},
+};
+
+export default config;
---
+++ scripts/setRegistry.sh
@@ -0,0 +1,3 @@
+echo 'Setting registry token';
+pnpm config set //registry.npmjs.org/:_authToken=${NPM_TOKEN};
+echo 'Registry token set';
