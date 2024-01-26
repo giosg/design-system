0a15ef8d3b2914cca5d9071b4d9e984860b479b5
Igor Kornilov <40828942+gab1991@users.noreply.github.com>
2024-01-26 13:28:16 +0200

    fix storybook linter (#5)
    * fix storybook linter fix
    
    * test lint pr
    
    * changes
    
    * Update package installation in .drone.yml
    
    * Enable corepack in install-packages and lint steps
    
    * Add failure: ignore to lint step and add everything-is-fine step
    
    * Remove lint step and move lint command to the lint step
    
    * Add linting step to build process
    
    * Add PNPM configuration and linting step
    
    * Refactor render function in Primary story
    
    * fix stylelint command
    
    * Remove unused CSS command from package.json

--- apps/storybook/.eslintrc.cjs
+++ apps/storybook/.eslintrc.cjs
@@ -1,4 +1,4 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-	extends: ["@repo/eslint-config/storybook.js", "plugin:storybook/recommended"],
+	extends: ["@repo/eslint-config/storybook.js", "plugin:mdx/recommended"],
 };
