3a2a7b9b3609e1ba1c99ef4357cc227318e0e7b6
Igor Kornilov <gaber32188@gmail.com>
2024-01-30 13:50:18 +0200

    add shadows generation

--- packages/foundations/postCssImport/main.js
+++ packages/foundations/postCssImport/main.js
@@ -7,34 +7,32 @@ const referencePlugin = require("../plugins/reference-plugin.js");
 const mergeDeclPlugin = require("../plugins/merge-decl-plugin.js");
 
 // Process a CSS file
-async function processCss(inputFile, outputFile, plugins = []) {
+async function processCss(inputFile, plugins = []) {
 	const css = fs.readFileSync(path.join(__dirname, inputFile), "utf-8");
 	const result = await postcss(plugins).process(css, {
 		from: inputFile,
 	});
-	fs.writeFileSync(path.join(__dirname, outputFile), result.css);
+
+	return result;
 }
 
 // Merge CSS declarations
 async function mergeDeclarations(filesToMerge, outputFile) {
-	const [file1, file2, file3] = filesToMerge;
-	const css1 = fs.readFileSync(path.join(__dirname, file1), "utf-8");
-	const css2 = fs.readFileSync(path.join(__dirname, file2), "utf-8");
-	const css3 = fs.readFileSync(path.join(__dirname, file3), "utf-8");
+	const combined = filesToMerge.join("");
 
-	const combined = css1 + css2 + css3;
 	const result = await postcss([mergeDeclPlugin()]).process(combined, {
 		from: combined,
 	});
-
 	fs.writeFileSync(path.join(__dirname, "..", outputFile), result.css);
 }
 
 // Execute the tasks
 async function execute() {
-	await processCss("reference.css", "reference-output.css", [normalizer(), referencePlugin()]);
-	await processCss("system.css", "system-output.css", [normalizer(), systemPlugin()]);
-	mergeDeclarations(["base.css", "reference-output.css", "system-output.css"], "index.css");
+	const refResult = await processCss("reference.css", [normalizer(), referencePlugin()]);
+	const sysResult = await processCss("system.css", [normalizer(), systemPlugin()]);
+	const baseCss = await processCss("base.css", [normalizer()]);
+
+	mergeDeclarations([baseCss, refResult, sysResult], "index.css");
 }
 
 execute().catch(console.error);
