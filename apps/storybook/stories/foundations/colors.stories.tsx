7eb388b156400a5a72d09f882c095add2e5495f4
Igor Kornilov <gaber32188@gmail.com>
2024-01-30 16:20:59 +0200

    Add alpha color palette to color module

--- apps/storybook/stories/foundations/colors.stories.tsx
+++ apps/storybook/stories/foundations/colors.stories.tsx
@@ -16,11 +16,19 @@ interface ColorCellProps {
 }
 
 const SolidColorPalette = ["Grey", "Purple", "Mint", "Pink", "Orange", "Red", "Green", "Yellow", "Blue"] as const;
+const AlphaColorPalette = ["Black", "White", "Purple", "Mint", "Orange", "Red", "Green", "Yellow", "Blue"] as const;
 
 const getSolidColorPaletteProp = (name: string): { prefix: string; gradation: number[] } => {
 	return {
 		prefix: `--gds-ref-color-solid-${name.toLocaleLowerCase()}-`,
 		gradation: [10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
+	};
+};
+
+const getAlphaColorPaletteProp = (name: string): { prefix: string; gradation: number[] } => {
+	return {
+		prefix: `--gds-ref-color-alpha-${name.toLocaleLowerCase()}-`,
+		gradation: [10, 20, 40, 60, 80],
 	};
 };
 
@@ -47,6 +55,9 @@ export const Palette: Story = {
 		const SolidColorMap = SolidColorPalette.map((colorName) => {
 			return { ...getSolidColorPaletteProp(colorName), name: colorName };
 		});
+		const AlphaColorMap = AlphaColorPalette.map((colorName) => {
+			return { ...getAlphaColorPaletteProp(colorName), name: colorName };
+		});
 
 		return (
 			<div className={styles.canvas}>
@@ -62,6 +73,18 @@ export const Palette: Story = {
 							<div className={styles.rowContainer} key={name}>
 								<h3>{name}</h3>
 								<div className={styles.row} key={name}>
+									{gradation.map((value) => {
+										return <ColorCell key={value} value={value} variablePrefix={prefix} />;
+									})}
+								</div>
+							</div>
+						);
+					})}
+					{AlphaColorMap.map(({ gradation, name, prefix }) => {
+						return (
+							<div className={styles.rowContainer} key={name}>
+								<h3>{name}</h3>
+								<div className={styles.alphaRow} key={name}>
 									{gradation.map((value) => {
 										return <ColorCell key={value} value={value} variablePrefix={prefix} />;
 									})}
