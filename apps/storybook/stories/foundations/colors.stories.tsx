29cc7f9afa2b3e02315705b54dc7350aa4609820
Igor Kornilov <gaber32188@gmail.com>
2024-01-31 15:15:24 +0200

    theme story

--- apps/storybook/stories/foundations/colors.stories.tsx
+++ apps/storybook/stories/foundations/colors.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import type { HTMLAttributes } from "react";
 import { ColorCell } from "./colorCell/colorCell";
-import styles from "./color.module.css";
+import styles from "./colors.module.css";
 
 const meta: Meta = {
 	title: "Foundations/Colors",
@@ -48,7 +49,7 @@ export const Palette: Story = {
 		});
 
 		return (
-			<div className={styles.canvas}>
+			<div className={styles.paletteContainer}>
 				<div className={styles.row}>
 					<div className={styles.cellContainer}>
 						<ColorCell label="White" variablePrefix="--gds-ref-color-solid-basics-white" />
@@ -82,4 +83,164 @@ export const Palette: Story = {
 			</div>
 		);
 	},
+};
+
+const ShortOptionList = ["strong", "medium", "mild"] as const;
+const FullOptionList = ["strongest", "stronger", "strong", "medium", "mild", "milder", "mildest"] as const;
+
+export const Themes: Story = {
+	parameters: {
+		options: { showPanel: false },
+		controls: { hideNoControlsWarning: true },
+		layout: "centered",
+	},
+	decorators: [
+		(Story) => {
+			document.body.classList.remove(styles.body);
+			return <Story />;
+		},
+	],
+	render: () => {
+		return (
+			<div className={styles.themes}>
+				<ThemeCanvas>
+					<h1 className={styles.themeHeader}>Light Theme</h1>
+				</ThemeCanvas>
+				<ThemeCanvas data-theme="dark">
+					<h1 className={styles.themeHeader}>Dark Theme</h1>
+				</ThemeCanvas>
+			</div>
+		);
+	},
+};
+
+function ThemeCanvas(props: HTMLAttributes<HTMLDivElement>) {
+	const { children, ...restProps } = props;
+	return (
+		<div className={styles.themeContainer} {...restProps}>
+			{children}
+			<div className={styles.section}>
+				<h1 className={styles.headerXXL}>Content</h1>
+				<div className={styles.row}>
+					<h3 className={styles.headerM}>Neutral</h3>
+					{generateCellContainer("--gds-sys-color-content-neutral-", [
+						"default",
+						"strong",
+						"medium",
+						"mild",
+						"milder",
+						"mildest",
+						"inverted",
+					])}
+				</div>
+				<div className={styles.horizontalRow}>
+					<div className={styles.row}>
+						<h3 className={styles.headerM}>Primary</h3>
+						{generateCellContainer("--gds-sys-color-content-color-primary-", ShortOptionList)}
+					</div>
+					<div className={styles.row}>
+						<h3 className={styles.headerM}>Tertiary</h3>
+						{generateCellContainer("--gds-sys-color-content-color-tertiary-", ShortOptionList)}
+					</div>
+				</div>
+				<div className={styles.horizontalRow}>
+					<div className={styles.row}>
+						<h3 className={styles.headerM}>Secondary</h3>
+						{generateCellContainer("--gds-sys-color-content-color-secondary-", ShortOptionList)}
+					</div>
+					<div className={styles.row}>
+						<h3 className={styles.headerM}>Quaternary</h3>
+						{generateCellContainer("--gds-sys-color-content-color-quaternary-", ShortOptionList)}
+					</div>
+				</div>
+			</div>
+			<div className={styles.section}>
+				<h1 className={styles.headerXXL}>Background</h1>
+				<div className={styles.row}>
+					<h3 className={styles.headerM}>Neutral</h3>
+					{generateCellContainer("--gds-sys-color-background-neutral-", [
+						"default",
+						"mildest",
+						"milder",
+						"mild",
+						"medium",
+						"strong",
+						"inverted",
+					])}
+				</div>
+				<div className={styles.row}>
+					<h3 className={styles.headerM}>Primary</h3>
+					{generateCellContainer("--gds-sys-color-background-primary-", FullOptionList)}
+				</div>
+				<div className={styles.row}>
+					<h3 className={styles.headerM}>Secondary</h3>
+
+					{generateCellContainer("--gds-sys-color-background-secondary-", FullOptionList)}
+				</div>
+				<div className={styles.row}>
+					<h3 className={styles.headerM}>Tertiary</h3>
+
+					{generateCellContainer("--gds-sys-color-background-tertiary-", FullOptionList)}
+				</div>
+				<div className={styles.row}>
+					<h3 className={styles.headerM}>Quaternary</h3>
+
+					{generateCellContainer("--gds-sys-color-background-quaternary-", FullOptionList)}
+				</div>
+			</div>
+			<div className={styles.section}>
+				<h1 className={styles.headerXXL}>Status</h1>
+				<div className={styles.row}>
+					<h3 className={styles.headerM}>Positive</h3>
+					{generateCellContainer("--gds-sys-color-status-positive-", FullOptionList)}
+				</div>
+				<div className={styles.row}>
+					<h3 className={styles.headerM}>Warning</h3>
+
+					{generateCellContainer("--gds-sys-color-status-warning-", FullOptionList)}
+				</div>
+				<div className={styles.row}>
+					<h3 className={styles.headerM}>Negative</h3>
+					{generateCellContainer("--gds-sys-color-status-negative-", FullOptionList)}
+				</div>
+				<div className={styles.row}>
+					<h3 className={styles.headerM}>Info</h3>
+					{generateCellContainer("--gds-sys-color-status-info-", FullOptionList)}
+				</div>
+			</div>
+			<div className={styles.section}>
+				<h1 className={styles.headerXXL}>Border</h1>
+				<div className={styles.row} style={{ width: "50%" }}>
+					<h3 className={styles.headerM}>Neutral</h3>
+					{generateCellContainer("--gds-sys-color-border-neutral-", ["inverted", "milder", "mild", "strong"])}
+				</div>
+				<div className={styles.row} style={{ width: "50%" }}>
+					<h3 className={styles.headerM}>Status</h3>
+					{generateCellContainer("--gds-sys-color-border-status-", ["info", "negative", "positive", "warning"])}
+				</div>
+				<div className={styles.row} style={{ width: "50%" }}>
+					<h3 className={styles.headerM}>State</h3>
+					{generateCellContainer("--gds-sys-color-border-state-", ["focused", "hovered", "pressed"])}
+				</div>
+			</div>
+			<div className={styles.section}>
+				<h1 className={styles.headerXXL}>Overlay</h1>
+				<div className={styles.row} style={{ width: "50%" }}>
+					<h3 className={styles.headerM}>Status</h3>
+					{generateCellContainer("--gds-sys-color-overlay-", ["tint", "mild", "strong"], true)}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+const generateCellContainer = (prefix: string, pallette: readonly (string | number)[], alpha?: boolean) => {
+	const containerStyles = alpha ? styles.alphaCellContainer : styles.cellContainer;
+	return (
+		<div className={containerStyles}>
+			{pallette.map((value) => {
+				return <ColorCell key={value} value={value} variablePrefix={prefix} />;
+			})}
+		</div>
+	);
 };
