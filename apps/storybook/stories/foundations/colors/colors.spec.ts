5ed0d9db424fcf4a07bb170b9d0e4380f2f5da4b
Igor Kornilov <gaber32188@gmail.com>
2024-02-08 16:09:10 +0200

    Update Playwright config and fix URLs in color tests

--- apps/storybook/stories/foundations/colors/colors.spec.ts
+++ apps/storybook/stories/foundations/colors/colors.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test("Palette", async ({ page }) => {
-	await page.goto("http://localhost:6006/iframe.html?args=&id=foundations-colors--palette&viewMode=story");
+	await page.goto("iframe.html?args=&id=foundations-colors--palette&viewMode=story");
 	const palette = page.getByTestId("palette");
 	await expect(palette).toHaveScreenshot("palette.png");
 });
 
 test("Themes", async ({ page }) => {
-	await page.goto("http://localhost:6006/iframe.html?args=&id=foundations-colors--themes&viewMode=story");
+	await page.goto("iframe.html?args=&id=foundations-colors--themes&viewMode=story");
 	const lightThemeContainer = page.getByTestId("light-theme");
 	const darkThemeContainer = page.getByTestId("dark-theme");
 	await expect(lightThemeContainer).toHaveScreenshot("light-theme.png");
