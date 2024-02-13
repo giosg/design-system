16b1033622d94d50de055e7f340ba0f48e54338d
Igor Kornilov <gaber32188@gmail.com>
2024-02-13 15:54:57 +0200

    Refactor CSS value transformation in manual.cjs

--- packages/foundations/manual.cjs
+++ packages/foundations/manual.cjs
@@ -8,11 +8,41 @@ function traverse(obj, prefix = "") {
 		if (typeof obj[key] === "object" && obj[key] !== null && !hasValue) {
 			traverse(obj[key], `${prefix}-${key}`);
 		} else if (hasValue) {
-			css += `--${prefix.slice(1)}: ${obj[key].value};\n`;
+			const value = obj[key].value;
+			try {
+				if (value.includes("{")) {
+					const changedValue = transformValue(value);
+					css += `--${prefix.slice(1)}: ${changedValue};\n`;
+				} else {
+					css += `--${prefix.slice(1)}: ${value};\n`;
+				}
+			} catch (err) {
+				console.log(err);
+				console.log(value);
+			}
 		}
 	}
 }
 
-traverse(obj["Core/Reference"]);
+function transformValue(value) {
+	return value.replace(/\{(.+?)\}/g, function (_, match) {
+		var parts = match.split(".");
+		return "var(--" + parts.join("-") + ")";
+	});
+}
+
+// --gds-sys-space: {gds.ref.space.0};
+// --gds-sys-space: {gds.ref.space.0,125};
+// --gds-sys-space: {gds.ref.space.0,25};
+// --gds-sys-space: {gds.ref.space.0,5};
+// --gds-sys-space: {gds.ref.space.0,75};
+// --gds-sys-space: {gds.ref.space.1};
+// --gds-sys-space: {gds.ref.space.1,25};
+// --gds-sys-space: {gds.ref.space.1,5};
+// --gds-sys-space: {gds.ref.space.1,75};
+// --gds-sys-space: {gds.ref.space.2};
+// --gds-sys-space: {gds.ref.space.2,5};
+
+traverse(obj["Core/System"]);
 
 console.log(css);
