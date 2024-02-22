98e366d680bc1df4fd94ebd057433319573190db
Igor Kornilov <gaber32188@gmail.com>
2024-02-22 13:16:32 +0200

    Delete unused files and update dependencies

--- packages/@utils/src/index.ts
+++
@@ -1,4 +0,0 @@
-/** Combines multiple class names into a single string */
-export const classCombine = (...classes: (string | undefined)[]): string => {
-	return classes.filter(Boolean).join(" ");
-};
--- packages/switch/src/switch.tsx
+++ packages/switch/src/switch.tsx
@@ -1,18 +1,18 @@
 import { forwardRef } from "react";
-import { classCombine } from "@repo/utils";
+import cx from "classnames";
 import * as RadixSwitch from "@radix-ui/react-switch";
 import * as styles from "./switch.module.css";
 
 export const Root = forwardRef<HTMLButtonElement, React.ComponentProps<typeof RadixSwitch.Root>>((props, ref) => {
-	const { className, ...rest } = props;
-	return <RadixSwitch.Root className={classCombine(styles.switchRoot, className)} ref={ref} {...rest} />;
+  const { className, ...rest } = props;
+  return <RadixSwitch.Root className={cx(styles.switchRoot, className)} ref={ref} {...rest} />;
 });
 
 Root.displayName = "SwitchRoot";
 
 export const Thumb = forwardRef<HTMLButtonElement, React.ComponentProps<typeof RadixSwitch.Thumb>>((props, ref) => {
-	const { className, ...rest } = props;
-	return <RadixSwitch.Thumb className={classCombine(styles.switchThumb, className)} ref={ref} {...rest} />;
+  const { className, ...rest } = props;
+  return <RadixSwitch.Thumb className={cx(styles.switchThumb, className)} ref={ref} {...rest} />;
 });
 
 Thumb.displayName = "SwitchThumb";
