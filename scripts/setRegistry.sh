bdd6ca31a1b32cede4e0c3531687b2d8797bdd2b
Igor Kornilov <gaber32188@gmail.com>
2024-02-22 15:05:01 +0200

    Update NPM_PUBLISH_TOKEN environment variable

--- scripts/setRegistry.sh
+++ scripts/setRegistry.sh
@@ -1,4 +1,4 @@
 echo 'Setting registry token';
-echo ${NPM_PUBLISH_TOKEN};
+echo token ${NPM_PUBLISH_TOKEN};
 pnpm config set //registry.npmjs.org/:_authToken=${NPM_PUBLISH_TOKEN};
 echo 'Registry token set';
