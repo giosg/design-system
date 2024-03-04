fee1b4ad389355daee6737c420f67624806e360d
Tomas Hiltunen <tomas.hiltunen@gmail.com>
2024-03-04 11:03:12 +0200

    Use soft comparator options only on arm architecture

--- apps/storybook/stories/foundations/box-shadows/box-shadows.spec.ts
+++ apps/storybook/stories/foundations/box-shadows/box-shadows.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from "@playwright/test";
-import { getSoftComparatorOptions } from "../../../utils/testUtils";
+import { getSoftComparatorOptionsOnArmArchitecture } from "../../../utils/testUtils";
 
 test("Box shadows", async ({ page, browserName }) => {
-  const softComparatorOptions = browserName === "firefox" ? getSoftComparatorOptions() : undefined;
+  const softComparatorOptions = browserName === "firefox" ? getSoftComparatorOptionsOnArmArchitecture() : undefined;
   await page.goto("iframe.html?args=&id=foundations-box-shadows--variants&viewMode=story");
   const palette = page.getByTestId("box-shadows");
   await expect(palette).toHaveScreenshot("box-shadows.png", softComparatorOptions);
--- apps/storybook/utils/testUtils.ts
+++ apps/storybook/utils/testUtils.ts
@@ -1,9 +1,8 @@
 /**
- * Returns soft comparator options with increased pixel difference threshold when running locally
- * to avoid flaky test screenshot results e.g. on Mac m1 architecture
+ * Returns soft comparator options with increased pixel difference threshold when running on arm architecture
  */
-export const getSoftComparatorOptions = (options?: Partial<Options>): Options => {
-  if (process.env.CI) {
+export const getSoftComparatorOptionsOnArmArchitecture = (options?: Partial<Options>): Options => {
+  if (!process.arch.includes("arm")) {
     return {
       maxDiffPixelRatio: 0,
       maxDiffPixels: 0,
@@ -19,11 +18,11 @@ export const getSoftComparatorOptions = (options?: Partial<Options>): Options =>
 
 interface Options {
   /**
-   * An acceptable ratio of pixels that are different to the total amount of pixels, between '0' and '1'.
+   * An acceptable ratio of pixels that are different to the total amount of pixels, between '0' and '1'
    */
   maxDiffPixelRatio: number;
   /**
-   * An acceptable amount of pixels that could be different.
+   * An acceptable amount of pixels that could be different
    */
   maxDiffPixels: number;
 }
