7d27615a40d125e28a75a06d09c2ca515b7c3fe4
Tomas Hiltunen <tomas.hiltunen@gmail.com>
2024-03-15 16:16:33 +0200

    Fix indentation

--- scripts/createTestScreenshotsArchive.sh
+++ scripts/createTestScreenshotsArchive.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [[ $1 == "--help" ]] || [[ $1 == "-h" ]]; then
-  echo "This script builds a zip archive from the current screenshots made by playwright";
+  echo "This script builds a tar archive from the screenshots made by playwright";
   exit 0;
 fi
 
 if [ ! -d "./apps/storybook/test-results" ]; then
-  echo "Directory with current screenshots not found in the file tree."
+  echo "Directory with tests screenshot results not found in the file tree."
   exit 1
 else
   mkdir ./apps/storybook/tmp
-  tar -czvf ./apps/storybook/tmp/current_screenshots.tar.gz ./apps/storybook/test-results || exit 1
-  echo "Done ./apps/storybook/tmp/current_screenshots.tar.gz with all current screenshots generated"
+  tar -czvf ./apps/storybook/tmp/screenshots.tar.gz ./apps/storybook/test-results || exit 1
+  echo "Done ./apps/storybook/tmp/screenshots.tar.gz with all screenshots generated"
 fi
 
