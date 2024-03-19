483d9cd51c17f6bc31b39e0bef9cc2642f8c94db
Tomas Hiltunen <tomas.hiltunen@gmail.com>
2024-03-19 10:50:37 +0200

    Update path to screenshots archive

--- scripts/createTestScreenshotsArchive.sh
+++ scripts/createTestScreenshotsArchive.sh
@@ -9,8 +9,7 @@ if [ ! -d "./apps/storybook/test-results" ]; then
   echo "Directory with tests screenshot results not found in the file tree."
   exit 1
 else
-  mkdir ./apps/storybook/tmp
-  tar -czvf ./apps/storybook/tmp/screenshots.tar.gz ./apps/storybook/test-results || exit 1
-  echo "Done ./apps/storybook/tmp/screenshots.tar.gz with all screenshots generated"
+  tar -czvf ./screenshots.tar.gz ./apps/storybook/test-results || exit 1
+  echo "Done screenshots.tar.gz with all screenshots generated"
 fi
 
