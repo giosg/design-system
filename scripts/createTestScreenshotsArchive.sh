9dfa8c870353ff95ed9c8431e309ce95c6868351
Tomas Hiltunen <tomas.hiltunen@gmail.com>
2024-03-19 11:09:02 +0200

    Remove one screenshot reference

--- scripts/createTestScreenshotsArchive.sh
+++ scripts/createTestScreenshotsArchive.sh
@@ -9,7 +9,7 @@ if [ ! -d "./apps/storybook/test-results" ]; then
   echo "Directory with tests screenshot results not found in the file tree."
   exit 1
 else
-  tar -czvf ./screenshots.tar.gz ./apps/storybook/test-results || exit 1
-  echo "Done screenshots.tar.gz with all screenshots generated"
+  tar -czvf playwright_screenshots.tar.gz ./apps/storybook/test-results || exit 1
+  echo "Done playwright_screenshots.tar.gz with all screenshots generated"
 fi
 
