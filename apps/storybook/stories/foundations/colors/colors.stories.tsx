3a0cc9ded7830642a0f37b5b43525d0e92a0c539
Tomas Hiltunen <tomas.hiltunen@gmail.com>
2024-04-21 21:20:25 +0300

    Update colors palette story

--- apps/storybook/stories/foundations/colors/colors.stories.tsx
+++ apps/storybook/stories/foundations/colors/colors.stories.tsx
@@ -16,60 +16,78 @@ export default meta;
 
 type Story = StoryObj;
 
-const SolidColorPalette = ["Grey", "Purple", "Mint", "Pink", "Orange", "Red", "Green", "Yellow", "Blue"] as const;
-const AlphaColorPalette = ["Black", "White", "Purple", "Mint", "Orange", "Red", "Green", "Yellow", "Blue"] as const;
+const SOLID_COLOR_GRADATION = [10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900] as const;
+const ALPHA_COLOR_GRADATION = [10, 20, 40, 60, 80] as const;
 
-const getSolidColorPaletteProp = (name: string) => {
-  return {
-    prefix: `--gds-ref-color-solid-${name.toLocaleLowerCase()}-`,
-    gradation: [10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900],
-  };
-};
+type SolidColorGradation = (typeof SOLID_COLOR_GRADATION)[number];
+type AlphaColorGradation = (typeof ALPHA_COLOR_GRADATION)[number];
 
-const getAlphaColorPaletteProp = (name: string) => {
+interface Color {
+  name: string;
+  gradation: Readonly<SolidColorGradation[] | AlphaColorGradation[]> | null;
+  token: string;
+  type: "solid" | "alpha";
+  label?: string;
+}
+
+const Colors: Color[] = [
+  { name: "Grey", gradation: SOLID_COLOR_GRADATION, token: "grey", type: "solid" },
+  { name: "Black/Alpha", gradation: ALPHA_COLOR_GRADATION, token: "black", type: "alpha" },
+  { name: "White/Alpha", gradation: ALPHA_COLOR_GRADATION, token: "white", type: "alpha" },
+  { name: "Purple", gradation: SOLID_COLOR_GRADATION, token: "purple", type: "solid" },
+  { name: "Purple/Alpha", gradation: ALPHA_COLOR_GRADATION, token: "purple", type: "alpha" },
+  { name: "Mint", gradation: SOLID_COLOR_GRADATION, token: "mint", type: "solid" },
+  { name: "Mint/Alpha", gradation: ALPHA_COLOR_GRADATION, token: "mint", type: "alpha" },
+  { name: "Pink", gradation: SOLID_COLOR_GRADATION, token: "pink", type: "solid" },
+  { name: "Pink/Alpha", gradation: ALPHA_COLOR_GRADATION, token: "pink", type: "alpha" },
+  { name: "Orange", gradation: SOLID_COLOR_GRADATION, token: "orange", type: "solid" },
+  { name: "Orange/Alpha", gradation: ALPHA_COLOR_GRADATION, token: "orange", type: "alpha" },
+  { name: "Red", gradation: SOLID_COLOR_GRADATION, token: "red", type: "solid" },
+  { name: "Red/Alpha", gradation: ALPHA_COLOR_GRADATION, token: "red", type: "alpha" },
+  { name: "Green", gradation: SOLID_COLOR_GRADATION, token: "green", type: "solid" },
+  { name: "Green/Alpha", gradation: ALPHA_COLOR_GRADATION, token: "green", type: "alpha" },
+  { name: "Yellow", gradation: SOLID_COLOR_GRADATION, token: "yellow", type: "solid" },
+  { name: "Yellow/Alpha", gradation: ALPHA_COLOR_GRADATION, token: "yellow", type: "alpha" },
+  { name: "Blue", gradation: SOLID_COLOR_GRADATION, token: "blue", type: "solid" },
+  { name: "Blue/Alpha", gradation: ALPHA_COLOR_GRADATION, token: "blue", type: "alpha" },
+  { name: "Transparent", gradation: null, token: "transparent", label: "Alpha", type: "alpha" },
+] as const;
+
+const getColorPaletteProp = (color: Color) => {
+  const { name, label, gradation, token, type } = color;
+  const prefix = `--gds-ref-color-${type}-${token}${gradation ? "-" : ""}`;
+  const tokenName = `ref.color.${type}.${token}`;
+
   return {
-    prefix: `--gds-ref-color-alpha-${name.toLocaleLowerCase()}-`,
-    gradation: [10, 20, 40, 60, 80],
+    name: label || name,
+    prefix,
+    tokenName,
+    gradation,
   };
 };
 
 export const Palette: Story = {
   render: () => {
-    const SolidColorMap = SolidColorPalette.map((colorName) => {
-      return { ...getSolidColorPaletteProp(colorName), name: colorName };
-    });
-    const AlphaColorMap = AlphaColorPalette.map((colorName) => {
-      return { ...getAlphaColorPaletteProp(colorName), name: colorName };
+    const ColorMap = Colors.map((color) => {
+      return { ...getColorPaletteProp(color) };
     });
 
     return (
       <div className={styles.paletteContainer} data-testid="palette">
-        <div className={styles.row}>
-          <div className={styles.cellContainer}>
-            <ColorCell label="White" variablePrefix="--gds-ref-color-solid-basics-white" />
-            <ColorCell label="Black" variablePrefix="--gds-ref-color-solid-basics-black" />
-          </div>
-        </div>
-        {SolidColorMap.map(({ gradation, name, prefix }) => {
+        {ColorMap.map(({ name, prefix, tokenName, gradation }) => {
           return (
-            <div className={styles.row} key={name}>
+            <div className={styles.colorWrapper} key={name}>
               <h3>{name}</h3>
               <div className={styles.cellContainer} key={name}>
-                {gradation.map((value) => {
-                  return <ColorCell key={value} value={value} variablePrefix={prefix} />;
-                })}
-              </div>
-            </div>
-          );
-        })}
-        {AlphaColorMap.map(({ gradation, name, prefix }) => {
-          return (
-            <div className={styles.row} key={name}>
-              <h3>Alpha {name}</h3>
-              <div className={styles.alphaCellContainer} key={name}>
-                {gradation.map((value) => {
-                  return <ColorCell key={value} value={value} variablePrefix={prefix} />;
-                })}
+                {gradation ? (
+                  gradation.map((value) => {
+                    return (
+                      <ColorCell key={value} label={`${tokenName}.${value}`} value={value} variablePrefix={prefix} />
+                    );
+                  })
+                ) : (
+                  <ColorCell key={tokenName} label={tokenName} variablePrefix={prefix} />
+                )}
               </div>
             </div>
           );
