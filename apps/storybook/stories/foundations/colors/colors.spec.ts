ac57f3589b43dbbf73331418d17ed26846220395
Igor Kornilov <gaber32188@gmail.com>
2024-04-29 16:06:43 +0300

    Update Storybook color tests

--- apps/storybook/stories/foundations/colors/colors.spec.ts
+++ apps/storybook/stories/foundations/colors/colors.spec.ts
@@ -1,17 +1,5 @@
 import { test, expect } from "@playwright/test";
 
-test("Emphasis", async ({ page }) => {
-  await page.goto("iframe.html?args=&id=foundations-colors--emphasis&viewMode=story");
-  const emphasis = page.getByTestId("emphasis");
-  await expect(emphasis).toHaveScreenshot("emphasis.png");
-});
-
-test("Palette", async ({ page }) => {
-  await page.goto("iframe.html?args=&id=foundations-colors--palette&viewMode=story");
-  const palette = page.getByTestId("palette");
-  await expect(palette).toHaveScreenshot("palette.png");
-});
-
 test("Themes", async ({ page }) => {
   await page.goto("iframe.html?args=&id=foundations-colors--themes&viewMode=story");
   const lightThemeContainer = page.getByTestId("light-theme");
