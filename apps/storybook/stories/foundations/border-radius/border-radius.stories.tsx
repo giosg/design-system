0838ba2a9572c27c52714d5c3dbfc9116de79ff3
Igor Kornilov <gaber32188@gmail.com>
2024-05-08 14:57:51 +0300

    refactor: Update container styles in themes module

--- apps/storybook/stories/foundations/border-radius/border-radius.stories.tsx
+++ apps/storybook/stories/foundations/border-radius/border-radius.stories.tsx
@@ -19,20 +19,35 @@ export const Variants: Story = {
   render: () => {
     return (
       <div className={styles.container} data-testid="border-radius">
-        <div className={styles.row}>
-          {["none", "3-xs", "2-xs", "xs", "s", "m", "l", "xl", "2-xl", "3-xl", "4-xl", "5-xl", "6-xl", "7-xl"].map(
-            (value) => {
-              return (
-                <div className={styles.itemContainer} key={value}>
-                  <VariableTooltip variableName={`--gds-sys-radius-${value}`}>
-                    <div className={styles.item} style={{ borderRadius: `var(--gds-sys-radius-${value})` }} />
-                  </VariableTooltip>
-                  <h3>{value.replace("-", "").toUpperCase()}</h3>
-                </div>
-              );
-            },
-          )}
-        </div>
+        {[
+          "none",
+          "5-xs",
+          "4-xs",
+          "3-xs",
+          "2-xs",
+          "xs",
+          "s",
+          "m",
+          "l",
+          "xl",
+          "2-xl",
+          "3-xl",
+          "4-xl",
+          "5-xl",
+          "6-xl",
+          "7-xl",
+          "8-xl",
+          "circle",
+        ].map((value) => {
+          return (
+            <div className={styles.itemContainer} key={value}>
+              <VariableTooltip variableName={`--gds-sys-radius-${value}`}>
+                <div className={styles.item} style={{ borderRadius: `var(--gds-sys-radius-${value})` }} />
+              </VariableTooltip>
+              <h3>{value.replace("-", "").toUpperCase()}</h3>
+            </div>
+          );
+        })}
       </div>
     );
   },
--- apps/storybook/stories/foundations/spacing/spacing.stories.tsx
+++ apps/storybook/stories/foundations/spacing/spacing.stories.tsx
@@ -19,23 +19,37 @@ export const Variants: Story = {
   render: () => {
     return (
       <div className={styles.container} data-testid="spacing">
-        <div className={styles.row}>
-          {["none", "3-xs", "2-xs", "xs", "s", "m", "l", "xl", "2-xl", "3-xl", "4-xl", "5-xl", "6-xl", "7-xl"].map(
-            (value) => {
-              return (
-                <div className={styles.itemContainer} key={value}>
-                  <VariableTooltip variableName={`--gds-sys-space-${value}`}>
-                    <div
-                      className={styles.item}
-                      style={{ width: `var(--gds-sys-space-${value})`, height: `var(--gds-sys-space-${value})` }}
-                    />
-                  </VariableTooltip>
-                  <h3>{value.replace("-", "").toUpperCase()}</h3>
-                </div>
-              );
-            },
-          )}
-        </div>
+        {[
+          "none",
+          "5-xs",
+          "4-xs",
+          "3-xs",
+          "2-xs",
+          "xs",
+          "s",
+          "m",
+          "l",
+          "xl",
+          "2-xl",
+          "3-xl",
+          "4-xl",
+          "5-xl",
+          "6-xl",
+          "7-xl",
+          "8-xl",
+        ].map((value) => {
+          return (
+            <div className={styles.itemContainer} key={value}>
+              <VariableTooltip variableName={`--gds-sys-space-${value}`}>
+                <div
+                  className={styles.item}
+                  style={{ width: `var(--gds-sys-space-${value})`, height: `var(--gds-sys-space-${value})` }}
+                />
+              </VariableTooltip>
+              <h3>{value.replace("-", "").toUpperCase()}</h3>
+            </div>
+          );
+        })}
       </div>
     );
   },
